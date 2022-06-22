9062d0baf2dbb76a1efd95ffca8f7f0a34a065a5
carol-profile <carolgitonga93@gmail.com>
2022-06-22 10:47:09 +0300

    deployment

--- neighbour/models.py
+++ neighbour/models.py
@@ -61,15 +61,6 @@ class Profile(models.Model):
     def save_profile(self):
         self.save()
 
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
-
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
-
     def __str__(self):
         return self.name
 
